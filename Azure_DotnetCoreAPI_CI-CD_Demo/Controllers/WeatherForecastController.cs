b671aaac18f73b12bb9a3c8edb4b74f2f27ce14f
w4560000 <39783609+w4560000@users.noreply.github.com>
2021-09-21 17:16:52 +0800

    set ci

--- Azure_DotnetCoreAPI_CI-CD_Demo/Controllers/WeatherForecastController.cs
+++ Azure_DotnetCoreAPI_CI-CD_Demo/Controllers/WeatherForecastController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Azure_DotnetCoreAPI_CI_CD_Demo.Controllers
 {
@@ -11,29 +9,25 @@ namespace Azure_DotnetCoreAPI_CI_CD_Demo.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        private readonly IConfiguration _configuration;
+
+        public WeatherForecastController(
+            ILogger<WeatherForecastController> logger,
+            IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public List<string> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return new List<string>()
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                $"環境:{_configuration["Env"]}", $"版本:1.0"
+            };
         }
     }
 }
