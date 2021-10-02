ab09f93ae9ff023a022cd97357603a0303c8a33f
w4560000 <39783609+w4560000@users.noreply.github.com>
2021-10-02 23:02:30 +0800

    upload test

--- Azure_DotnetCoreAPI_CI-CD_Demo/Controllers/WeatherForecastController.cs
+++ Azure_DotnetCoreAPI_CI-CD_Demo/Controllers/WeatherForecastController.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Azure_DotnetCoreAPI_CI_CD_Demo.Controllers
 {
@@ -28,6 +29,15 @@ namespace Azure_DotnetCoreAPI_CI_CD_Demo.Controllers
             {
                 $"環境:{_configuration["Env"]}", $"版本:1.1"
             };
+        }
+
+        [HttpGet("WriteToFile")]
+        public async Task WriteToFile()
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(System.IO.File.Create("Upload/test.txt")))
+            {
+                await file.WriteAsync("your text here");
+            }
         }
     }
 }
